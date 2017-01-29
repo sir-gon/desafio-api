cd9840a79caf77f9231709ad0b433fc355131b9e
Gonzalo Diaz Cruz <devel@gon.cl>
2017-01-29 17:40:04 -0300

    Ejemplo de recurso consumible como JSON

--- src/main/java/cl/gon/api/desafio/Main.java
+++ src/main/java/cl/gon/api/desafio/Main.java
@@ -2,10 +2,19 @@ package cl.gon.api.desafio;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.moxy.json.MoxyJsonConfig;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.ext.ContextResolver;
+
+
 
 /**
  * Main class.
@@ -13,20 +22,35 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://0.0.0.0:8080/desafio/";
+    public static final String BASE_URI = "http://0.0.0.0:8080/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in cl.gon.api.desafio package
-        final ResourceConfig rc = new ResourceConfig().packages("cl.gon.api.desafio");
+      try {
+          System.out.println("JSON with MOXy Jersey Example App");
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+          final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), createApp());
+          Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+              @Override
+              public void run() {
+                  server.shutdownNow();
+              }
+          }));
+          server.start();
+
+          System.out.println(String.format("Application started.%nStop the application using CTRL+C"));
+
+          Thread.currentThread().join();
+
+          return server;
+      } catch (IOException | InterruptedException ex) {
+          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+      }
+
+      return null;
     }
 
     /**
@@ -37,4 +61,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
     }
+
+    public static ResourceConfig createApp() {
+        return new ResourceConfig()
+                .packages("cl.gon.api.desafio")
+                .register(createMoxyJsonResolver());
+    }
+
+    public static ContextResolver<MoxyJsonConfig> createMoxyJsonResolver() {
+      final MoxyJsonConfig moxyJsonConfig = new MoxyJsonConfig();
+      Map<String, String> namespacePrefixMapper = new HashMap<String, String>(1);
+      namespacePrefixMapper.put("http://www.w3.org/2001/XMLSchema-instance", "xsi");
+      moxyJsonConfig.setNamespacePrefixMapper(namespacePrefixMapper).setNamespaceSeparator(':');
+      return moxyJsonConfig.resolver();
+  }
 }
